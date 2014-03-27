3b5e5d67a781cbb2d01ce013a38026a12a1fd091
Stephen Ian Advincula <dev@sylv3rblade.com>
2014-03-27 09:18:15 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+Time Track
+=========
+
+Time Track is a web-application for simple timetracking.
+
+Basic features include:
+  - Per Project organization of tasks
+  - Individual Task Tracking
+  - TBA
+
+Please note:
+
+> This project is meant to be a Test/Study Project for Indinero developers.
+> Please use at your own risk.  Support is at code-level only.
+
+Ruby version
+> 2.0.0-p451
+
+Rails version
+> 4.0.4
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimeTrack::Application.config.secret_key_base = '259010cf94c6d4bbb62aa4d9731e95cc4fa0265eadd87fc558a23425dd12aac2c40bbb56ff070b95d875005ff6bccdbc3af4d56b1240aeabf0a86dff17c1da78'
