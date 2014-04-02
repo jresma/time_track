1859065d2f9fab269a1ad4481f127ec42c534fd3
Jossie Resma <jorez18@gmail.com>
2014-04-02 11:27:46 +0800

    Added the controller test page of registration.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe User do
   context 'rails validations and constraints' do
-  	 let(:user) {User.new}
+  	let(:user) { User.new }
 
-  	 before(:each) do
-		user.valid?
-  	 end
+  	before(:each) do
+		  user.valid?
+  	end
 
-  	 it 'should have errors when validating' do
+  	it 'should have errors when validating' do
   	 	expect(user.errors).to include :firstname
   	 	expect(user.errors).to include :lastname
   	 	expect(user.errors).to include :email
